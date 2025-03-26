7641212aa65c9d240762216e2fc4bdb0d50863b8
divyavibi <divyav1122@gmail.com>
2025-03-26 11:02:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+console.log("Hello fromm the JavaScript file!");
---
+++ script3.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        var wordArray = [["James","Jill","Julia"],["sleeps","runs","smokes"]];
+        console.log ("I have heard that "+ wordArray[0][0]+""+ wordArray[1][2]);
+    </script>
+</body>
+</html>
